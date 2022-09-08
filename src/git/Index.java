de8d849e3756b7a307f6760bf2118b9a76e8b549
Naalah Cohen <naalah28@gmail.com>
2022-09-08 12:41:58 -0700

    added a rewrite for index

--- src/git/Index.java
+++ src/git/Index.java
@@ -1,16 +1,20 @@
 package git;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 
 public class Index {
 	FileWriter index; //table of contents, list on every signle item, and where it exists 
-	HashMap objects; 
+	HashMap <String, Blob> objects; 
 	public Index() throws IOException {
 		index = new FileWriter("index"); 
 		objects = new HashMap <String,Blob> ();
-		
+		File theDir = new File("/path/objects");
+		if (!theDir.exists()){
+		    theDir.mkdirs();
+		}
 		
 	}
 	public void initialize() {
@@ -22,11 +26,18 @@ public class Index {
 		index.append(fileName + " : " + newBlobWithFileName); 
 	}
 	
+	public void writeAllOfIndex () throws IOException {
+		for(String str : objects.keySet()) {
+			index.append(str + " : " ); 
+			index.append(" : " + objects.get(str).getSha());
+		}
+	}
+	
 	 
 	public void removeBlob(String fileName) throws IOException {
 		objects.remove(fileName);
-		FileWriter fileToDelete = new FileWriter("./objects/" + objects.get(fileName).getSha());
-		fileToDelete.close(); 
-	}
+		writeAllOfIndex();
+		FileWriter fileToDelete = new FileWriter("./objects/"+ objects.get(fileName).getSha());
+		fileToDelete.close(); 	}
 	
 }
