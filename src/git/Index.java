f4d8a3cefcc64370b4dcbdac288f6f3a03a5faa1
Naalah Cohen <naalah28@gmail.com>
2022-09-08 11:47:58 -0700

    added gitignore

--- src/git/Index.java
+++ src/git/Index.java
@@ -1,18 +1,24 @@
 package git;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.util.HashMap;
 
 public class Index {
-	File emptyFile = new File(null); 
-	HashMap objects = new HashMap (String,String); 
+	FileWriter index = new FileWriter(null); 
+	HashMap objects = new HashMap (String ,Blob); 
 	public Index() {
-		
+		FileWriter index = new FileWriter(null); 
 		
 	}
 	public void addBlob(String fileName) {
 		Blob newBlobWithFileName = new Blob (fileName);
-		objects.add()
+		objects.put(fileName,newBlobWithFileName);
+		index.append(fileName + newBlobWithFileName); 
+	}
+	public void removeBlob(String fileName) {
+		objects.remove(fileName);
+		
 	}
 	
 }
