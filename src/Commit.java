a2a66c0e046316c042ccccbb82dee9dfea25dd9e
Naalah Cohen <naalah28@gmail.com>
2022-09-19 21:34:58 -0700

    added some private instance variables to the contrsuctor

--- src/Commit.java
+++ src/Commit.java
@@ -1,15 +1,28 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import git.Tree;
+
 public class Commit {
-	public Commit() {
+	private Tree pTree; 
+	private String author1;
+	private String date;
+	private String summary;
+	private String nameOfFile; 
+	public Commit(String author, String pTreeValue, String sumOfChanges, String pointerParent) {
+		author1 = author;
+		summary = sumOfChanges; 
+		date = getDate (); 
+		
 		
 	}
 	
-	public String generateSha1 (String str) {
-		String value = str;
+	public String generateSha1 (String summary, String date, String author, String pointer) {
+		String value = summary + date + author + pointer;
 		String sha1 = "";
 		try {
 			MessageDigest digest = MessageDigest.getInstance("SHA-1");
@@ -26,5 +39,14 @@ public class Commit {
 	public String getDate () {
 		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
 		return timeStamp;
+	}
+	
+	public void writeFile () {
+		FileWriter fw = new FileWriter(new File("objects", str + ".txt"));
+		for(String stringe: arr) {
+		  fw.write(str + System.lineSeparator());
+		}
+		fw.close();
+		
 	}
 }
