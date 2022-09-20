881e3990996a17c7a65445fc7bd7f80eddcda408
Naalah Cohen <naalah28@gmail.com>
2022-09-19 22:59:24 -0700

    added methods to FileUtil

--- src/FileUtil.java
+++ src/FileUtil.java
@@ -1,4 +1,41 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 
 public class FileUtil {
+	public FileUtil () {
+		
+	}
+	
+	public static String getContent (String filepath) throws IOException {
+		File file = new File (filepath);
+		BufferedReader br = new BufferedReader(new FileReader(file)); 
+		StringBuilder sb = new StringBuilder(); 
+		String line = br.readLine(); 
+		while (line != null) { 
+			sb.append(line).append("\n"); 
+			line = br.readLine(); 
+		} 
+		String fileAsString = sb.toString();
+		return fileAsString;
+	} 
+	
+	
+	public static String sha1(String str) {
+		String value = str;
+		String sha1 = "";
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-1");
+	        digest.reset();
+	        digest.update(value.getBytes("utf8"));
+	        sha1 = String.format("%040x", new BigInteger(1, digest.digest()));
+		} catch (Exception e){
+			e.printStackTrace();
+		}
 
+		return sha1;
+	}
 }
