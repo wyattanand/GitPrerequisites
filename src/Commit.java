3cbad2abf9580b2792ee6b34a01e7196ebabd5a2
Naalah Cohen <naalah28@gmail.com>
2022-09-19 21:38:37 -0700

    wrote most of write file

--- src/Commit.java
+++ src/Commit.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.text.SimpleDateFormat;
@@ -8,16 +9,20 @@ import java.util.Calendar;
 import git.Tree;
 
 public class Commit {
-	private Tree pTree; 
+	private String pTree; 
 	private String author1;
 	private String date;
 	private String summary;
 	private String nameOfFile; 
+	private String sha1;
+	private String pointer;
 	public Commit(String author, String pTreeValue, String sumOfChanges, String pointerParent) {
 		author1 = author;
 		summary = sumOfChanges; 
 		date = getDate (); 
-		
+		pointer = pointerParent;
+		sha1 = generateSha1(summary,date,author1,pointer);
+		pTree = pTreeValue;
 		
 	}
 	
@@ -41,11 +46,14 @@ public class Commit {
 		return timeStamp;
 	}
 	
-	public void writeFile () {
-		FileWriter fw = new FileWriter(new File("objects", str + ".txt"));
-		for(String stringe: arr) {
-		  fw.write(str + System.lineSeparator());
-		}
+	public void writeFile () throws IOException {
+		FileWriter fw = new FileWriter(new File("objects", sha1));
+		fw.write(pTree); // 1 writing p Tree value 
+		fw.write("idk"); // 2 writing location of previous commit
+		fw.write("idk2"); // 3 writing location of next commit
+		fw.write(author1); //  4 writing author
+		fw.write(date); // 5 writing date
+		fw.write(summary); // 6 writing location of previous commit
 		fw.close();
 		
 	}
